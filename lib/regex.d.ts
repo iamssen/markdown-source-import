3fee2c1ece66924276e6a966fea7c7a84d50ae3b
SSen <i@ssen.name>
2019-12-21 00:15:45 +0900

    1.3.0

--- lib/regex.d.ts
+++ lib/regex.d.ts
@@ -1,9 +1,16 @@
-export declare const startPattern: RegExp;
-export declare const endPattern: RegExp;
+export declare const importStartPattern: RegExp;
+export declare const importEndPattern: RegExp;
 export declare const slicePattern: RegExp;
-export interface ImportMatch {
+export declare const indexStartPattern: RegExp;
+export declare const indexEndPattern: RegExp;
+export interface ImportParams {
     pattern: string;
     slice: [number, number] | undefined;
     titleTag: string | undefined;
 }
-export declare function match(line: string): ImportMatch | undefined;
+export interface IndexParams {
+    pattern: string;
+    tree: boolean;
+}
+export declare function matchImportStart(line: string): ImportParams | undefined;
+export declare function matchIndexStart(line: string): IndexParams | undefined;
